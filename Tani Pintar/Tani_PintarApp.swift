4cb8ca20eacf17ed8912cc9549f17050a46baa79
Haykal Mahdiy <haykalmahdiy@gmail.com>
2026-01-08 14:40:23 +0700

    Initial Commit

---
+++ Tani Pintar/Tani_PintarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tani_PintarApp.swift
+//  Tani Pintar
+//
+//  Created by Haykal on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Tani_PintarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
